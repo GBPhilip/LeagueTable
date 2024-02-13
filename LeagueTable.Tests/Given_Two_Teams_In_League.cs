27ae2f37012e7e114b20dfc88534ac76bbade518
Philip Sutton <GBPhilipSutton@gmail.com>
2024-02-13 19:08:28 +0000

    Add result to unit test (test in progress)

--- LeagueTable.Tests/Given_Two_Teams_In_League.cs
+++ LeagueTable.Tests/Given_Two_Teams_In_League.cs
@@ -8,11 +8,22 @@ namespace LeagueTableTests
     public class Given_Two_Teams_In_League
     {
         [Fact]
-        public void And_Played_Once_Home_Team_Beaten_Away_Team_Should_Return_Table_With_Home_Team_Top()
+        public void And_Played_Once_Home_Beats_Away_Should_Return_Table_With_Home_Top()
         {
+            var homeTeam = 1;
+            var awayTeam = 2;
+            var matchResult = new Result() 
+            {
+                HomeTeamId = homeTeam,
+                HomeScore = 1,
+                AwayTeamId = awayTeam,
+                AwayScore = 0,
+                Type = ResultEnum.Regulation
+            };
+        
             var topTeam = new TableEntry
             {
-                TeamId = 1,
+                TeamId = homeTeam,
                 Wins = 1,
                 Losses = 0,
                 OvertimeLosses = 0,
@@ -22,18 +33,18 @@ namespace LeagueTableTests
             };
             var bottomTeam = new TableEntry
             {
-                TeamId = 1,
-                Wins = 1,
-                Losses = 0,
+                TeamId = awayTeam,
+                Wins = 0,
+                Losses = 1,
                 OvertimeLosses = 0,
                 OvertimeWins = 0,
-                GoalsConceded = 0,
-                GoalsScored = 1
+                GoalsConceded = 1,
+                GoalsScored = 0
             };
             var expectedTable = new List<TableEntry> { topTeam, bottomTeam };
             var sut = new TableCalculator();
 
-            var result = sut.xxx(new List<Result>());
+            var result = sut.Sort(new List<Result>());
             
             result.Should().BeEquivalentTo(expectedTable, options => options.WithStrictOrdering());
         }
