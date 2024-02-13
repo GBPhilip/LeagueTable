d95f063dfbc7ce3700b0deac5a36f56881686de3
Philip Sutton <GBPhilipSutton@gmail.com>
2024-02-13 19:04:17 +0000

    Update formatting and set enum for overtime to 1

--- LeagueTable/Domain/ResultEnum.cs
+++ LeagueTable/Domain/ResultEnum.cs
@@ -10,11 +10,11 @@ namespace LeagueTable.Domain
         /// <summary>
         /// The Regulation value should be used when the result occurred within regulation time
         /// </summary>
-        public static  readonly ResultEnum Regulation = new (nameof(Regulation), 0);
+        public static readonly ResultEnum Regulation = new (nameof(Regulation), 0);
         /// <summary>
         /// The Overtime value should be used when the result occurred within overtime or through penalty shoots
         /// </summary>
-        public static  readonly ResultEnum Overtime = new (nameof(Overtime), 0);
+        public static readonly ResultEnum Overtime = new (nameof(Overtime), 1);
 
         private ResultEnum(string name, int value): base(name, value) { }
     }
