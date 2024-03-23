288c8499320f505c6d9c48b1d554ce73b19a8738
Philip Sutton <GBPhilipSutton@gmail.com>
2024-03-23 22:19:35 +0000

    Refatctor to have method to define away win, add failing test to count wins, with one home and 1 away win

--- LeagueTable.Tests/WinsTests.cs
+++ LeagueTable.Tests/WinsTests.cs
@@ -14,39 +14,68 @@ namespace LeagueTable.Tests
         {
             // Arrange
             var results = new List<Result>()
-            {
+            { 
                 HomeTeamRegulationWin()
             };
 
             var tableEntry = new TableEntry(2, results);
 
-           // Assert
+            // Assert
             tableEntry.Wins.Should().Be(1);
         }
+
         [Fact]
         public void When_Home_Score_Is_Greater_Than_Away_Score_And_Type_Is_Regulation_Should_Return_No_Wins_For_Away_Team()
         {
             // Arrange
             var results = new List<Result>()
             {
-                new Result()
-        private static Result HomeTeamRegulationWin()
-                {
-            return new Result()
-            {
-                    AwayTeamId = 1,
-                    AwayScore = 0,
-                    HomeTeamId = 2,
-                    HomeScore = 1,
-                    Type = ResultEnum.Regulation
-            };
-                }
+                HomeTeamRegulationWin()
             };
 
             var tableEntry = new TableEntry(1, results);
 
             // Assert
             tableEntry.Wins.Should().Be(0);
+        }
+
+        [Fact]
+        public void When_Team_Plays_One_Home_And_One_Away_And_Scores_More_Goals_In_Both_Team_Should_Have_Two_Wins()
+        {
+            // Arrange
+            var results = new List<Result>()
+            {
+                HomeTeamRegulationWin(),
+                AwayTeamRegulationWin()
+            };
+
+            var tableEntry = new TableEntry(2, results);
+
+            // Assert
+            tableEntry.Wins.Should().Be(0);
+        }
+
+        private static Result HomeTeamRegulationWin()
+        {
+            return new Result()
+            {
+                AwayTeamId = 1,
+                AwayScore = 0,
+                HomeTeamId = 2,
+                HomeScore = 1,
+                Type = ResultEnum.Regulation
+            };
+        }
+        private static Result AwayTeamRegulationWin()
+        {
+            return new Result()
+            {
+                AwayTeamId = 2,
+                AwayScore = 1,
+                HomeTeamId = 1,
+                HomeScore = 0,
+                Type = ResultEnum.Regulation
+            };
         }
     }
 }
