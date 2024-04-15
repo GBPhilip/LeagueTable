4fd6b8d99b743b6ee9538898c73690add6629384
chris <chris.leicester@cocoashed.com>
2024-04-16 00:45:27 +0100

    Add Test to check counting a regulation loss and an overtime loss is correct

--- LeagueTable.Tests/LossesTests.cs
+++ LeagueTable.Tests/LossesTests.cs
@@ -33,7 +33,7 @@ namespace LeagueTable.Tests
                 HomeTeamRegulationWin()
             };
 
-            var tableEntry = new TableEntry(1, results);
+            var tableEntry = new TableEntry(RED_TEAM, results);
 
             // Assert
             tableEntry.Losses.Should().Be(2);
@@ -41,6 +41,21 @@ namespace LeagueTable.Tests
         const int RED_TEAM = 1;
         const int BLUE_TEAM = 2;
         
+        [Fact]
+        public void When_Home_Score_Is_Greater_Than_Away_Score_Once_In_Regulation_Once_In_Overtime_Should_Return_Two_Loss_For_Away_Team()
+        {
+            // Arrange
+            var results = new List<Result>()
+            { 
+                HomeTeamRegulationWin(),
+                HomeTeamOvertimeWin()
+            };
+
+            var tableEntry = new TableEntry(RED_TEAM, results);
+
+            // Assert
+            tableEntry.Losses.Should().Be(2);
+        }
         private static Result HomeTeamRegulationWin()
         {
             return new Result()
@@ -52,6 +67,17 @@ namespace LeagueTable.Tests
                 Type = ResultEnum.Regulation
             };
         }  
-    }      
+        private static Result HomeTeamOvertimeWin()
+        {
+            return new Result()
+            {
+                AwayTeamId = RED_TEAM,
+                AwayScore = 0,
+                HomeTeamId = BLUE_TEAM,
+                HomeScore = 1,
+                Type = ResultEnum.Overtime
+            };
+        }  
 
+    }
 }
