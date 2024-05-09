7fee675f416170f960c1948ab73710b4018bb537
chris <chris.leicester@cocoashed.com>
2024-05-09 08:48:02 +0100

    Add test for multiple overtime wins

--- LeagueTable.Tests/OvertimeWinsTests.cs
+++ LeagueTable.Tests/OvertimeWinsTests.cs
@@ -29,6 +29,24 @@ namespace LeagueTable.Tests
             tableEntry.OvertimeWins.Should().Be(1);
         }
 
+        [Fact]
+        public void When_Home_Score_Is_Greater_Than_Away_Score_And_Type_Is_Overtime_Twice_Should_Return_Two_OvertimeWins_For_Home_Team()
+        {
+            // Arrange
+            var results = new List<Result>()
+            {
+                HomeTeamOvertimeWin(),
+                HomeTeamOvertimeWin()
+            };
+            var homeTeam = BLUE_TEAM;
+
+            //Act
+            var tableEntry = new TableEntry(homeTeam, results);
+
+            // Assert
+            tableEntry.OvertimeWins.Should().Be(2);
+        }
+
         private static Result HomeTeamOvertimeWin()
         {
             return new Result()
