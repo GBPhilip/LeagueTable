5ad0f407320ee76100bf97f2c11d5f238e2184bb
chris <chris.leicester@cocoashed.com>
2024-06-04 00:05:14 +0100

    Add new OvertimeLosses test  When_Home_Score_Is_Greater_Than_Away_Score_And_Type_Is_Overtime_Twice_Should_Return_Two_OvertimeLosses_For_Away_Team()

--- LeagueTable.Tests/OvertimeLossesTests.cs
+++ LeagueTable.Tests/OvertimeLossesTests.cs
@@ -29,6 +29,25 @@ namespace LeagueTable.Tests
             tableEntry.OvertimeLosses.Should().Be(1);
         }
 
+        [Fact]
+        public void When_Home_Score_Is_Greater_Than_Away_Score_And_Type_Is_Overtime_Twice_Should_Return_Two_OvertimeLosses_For_Away_Team()
+        {
+            // Arrange
+            var results = new List<Result>()
+            {
+                HomeTeamOvertimeWin(),
+                HomeTeamOvertimeWin()
+            };
+            var homeTeam = BLUE_TEAM;
+
+            //Act
+            var tableEntry = new TableEntry(homeTeam, results);
+
+            // Assert
+            tableEntry.OvertimeLosses.Should().Be(2);
+        }
+
+
         private static Result HomeTeamOvertimeWin()
         {
             return new Result()
