52295e55f1776d382ca86bdd16aa9566eb1ffd53
chris <chris.leicester@cocoashed.com>
2024-06-05 17:41:06 +0100

    Correct team id of SUT

--- LeagueTable.Tests/OvertimeLossesTests.cs
+++ LeagueTable.Tests/OvertimeLossesTests.cs
@@ -20,10 +20,10 @@ namespace LeagueTable.Tests
             {
                 HomeTeamOvertimeWin()
             };
-            var homeTeam = BLUE_TEAM;
+            var awayTeam = RED_TEAM;
 
             //Act
-            var tableEntry = new TableEntry(homeTeam, results);
+            var tableEntry = new TableEntry(awayTeam, results);
 
             // Assert
             tableEntry.OvertimeLosses.Should().Be(1);
