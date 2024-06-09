425ccec4db3e5849ddf9e5bfcb5afb7525c220d8
chris <chris.leicester@cocoashed.com>
2024-06-09 18:39:46 +0100

    Add xml comments to remaining 7 result filters Reorder overtime filter methods to match regulation time   filter methods

--- LeagueTable/Domain/TableEntryFilters.cs
+++ LeagueTable/Domain/TableEntryFilters.cs
@@ -6,7 +6,7 @@
         /// Filter to return home wins in regulation time for the specified team from the results
         /// </summary>
         /// <param name="results">The set of results to filter</param>
-        /// <param name="teamId">The id of the team that the home wins in regulation time</param>
+        /// <param name="teamId">The id of the team that the home wins in regulation time results are filtered for</param>
         /// <returns>The filtered results</returns>
         internal static IEnumerable<Result> HomeWinsInRegulation(this IEnumerable<Result> results, int teamId)
         {
@@ -16,6 +16,12 @@
                 && x.Type == ResultEnum.Regulation);
         }
 
+        /// <summary>
+        /// Filter to return away wins in regulation time for the specified team from the results
+        /// </summary>
+        /// <param name="results">The set of results to filter</param>
+        /// <param name="teamId">The id of the team that the away wins in regulation time results are filtered for</param>
+        /// <returns>The filtered results</returns>
         internal static IEnumerable<Result> AwayWinsInRegulation(this IEnumerable<Result> results, int teamId)
         {
             return results.Where(x =>
@@ -24,6 +30,12 @@
                 && x.Type == ResultEnum.Regulation);
         }
 
+        /// <summary>
+        /// Filter to return home wins in overtime time for the specified team from the results
+        /// </summary>
+        /// <param name="results">The set of results to filter</param>
+        /// <param name="teamId">The id of the team that the home wins in overtime time results are filtered for</param>
+        /// <returns>The filtered results</returns>
         internal static IEnumerable<Result> HomeWinsInOvertime(this IEnumerable<Result> results, int teamId)
         {
             return results.Where(x =>
@@ -32,6 +44,12 @@
                 && x.Type == ResultEnum.Overtime);
         }
 
+        /// <summary>
+        /// Filter to return away wins in overtime time for the specified team from the results
+        /// </summary>
+        /// <param name="results">The set of results to filter</param>
+        /// <param name="teamId">The id of the team that the away wins in overtime time results are filtered for</param>
+        /// <returns>The filtered results</returns>
         internal static IEnumerable<Result> AwayWinsInOvertime(this IEnumerable<Result> results, int teamId)
         {
             return results.Where(x =>
@@ -39,6 +57,13 @@
                 && x.AwayTeamId == teamId
                 && x.Type == ResultEnum.Overtime);
         }
+
+        /// <summary>
+        /// Filter to return home losses in regulation time for the specified team from the results
+        /// </summary>
+        /// <param name="results">The set of results to filter</param>
+        /// <param name="teamId">The id of the team that the home losses in regulation time results are filtered for</param>
+        /// <returns>The filtered results</returns>
         internal static IEnumerable<Result> HomeLossesInRegulation(this IEnumerable<Result> results, int teamId)
         {
             return results.Where(x =>
@@ -47,6 +72,26 @@
                 && x.Type == ResultEnum.Regulation);
         }
 
+        /// <summary>
+        /// Filter to return away losses in regulation time for the specified team from the results
+        /// </summary>
+        /// <param name="results">The set of results to filter</param>
+        /// <param name="teamId">The id of the team that the away losses in regulation time results are filtered for</param>
+        /// <returns>The filtered results</returns>
+        internal static IEnumerable<Result> AwayLossesInRegulation(this IEnumerable<Result> results, int teamId)
+        {
+            return results.Where(x =>
+                   x.HomeScore > x.AwayScore
+                && x.AwayTeamId == teamId
+                && x.Type == ResultEnum.Regulation);
+        }
+
+        /// <summary>
+        /// Filter to return home losses in overtime time for the specified team from the results
+        /// </summary>
+        /// <param name="results">The set of results to filter</param>
+        /// <param name="teamId">The id of the team that the home losses in overtime time results are filtered for</param>
+        /// <returns>The filtered results</returns>
         internal static IEnumerable<Result> HomeLossesInOvertime(this IEnumerable<Result> results, int teamId)
         {
             return results.Where(x =>
@@ -55,13 +100,12 @@
                 && x.Type == ResultEnum.Overtime);
         }
 
-        internal static IEnumerable<Result> AwayLossesInRegulation(this IEnumerable<Result> results, int teamId)
-        {
-            return results.Where(x =>
-                   x.HomeScore > x.AwayScore
-                && x.AwayTeamId == teamId
-                && x.Type == ResultEnum.Regulation);
-        }
+        /// <summary>
+        /// Filter to return away losses in overtime time for the specified team from the results
+        /// </summary>
+        /// <param name="results">The set of results to filter</param>
+        /// <param name="teamId">The id of the team that the away losses in overtime time results are filtered for</param>
+        /// <returns>The filtered results</returns>
         internal static IEnumerable<Result> AwayLossesInOvertime(this IEnumerable<Result> results, int teamId)
         {
             return results.Where(x =>
